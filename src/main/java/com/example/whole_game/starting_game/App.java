d6a3c1719e0a11939c3d8e54c2fbeb849947f649
Nazar <nazar20050716@gmail.com>
2023-10-03 17:45:00 +0200

    refactored finished code

---
+++ src/main/java/com/example/whole_game/starting_game/App.java
@@ -0,0 +1,12 @@
+package com.example.whole_game.starting_game;
+
+import com.example.whole_game.game_logic.Game;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
+}
